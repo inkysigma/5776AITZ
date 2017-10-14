51472d16e86bfc6b0046025f7846a3c6728d113d
Chaitanya Nookala <chaitanya.nookala@outlook.com>
2017-10-13 20:08:26 -0700

    Implemented mogo forward and backward

--- motor.h
+++ motor.h
@@ -1,5 +1,10 @@
 #include "corrector.c"
-
+const OUT_ANGLE = 90;
+const IN_ANGLE = 0;
+const FAST_SPEED = 100;
+const SLOW_ANGLE = 45;
+const SLOW_SPEED = 50
+const SYNC_SPEED = 15;
 void moveDrive (int left, int right) {
 	motor[LeftDrive] = left;
 	motor[RightDrive] = -right;
@@ -42,18 +47,53 @@ void stopClaw() {
 
 void openClawFully();
 void closeClawFully();
-
-void moveGoal(int power) {
+void moveLeftMogo(int power) {
 	int output = getMogoCorrection(power);
 	motor[LeftMobileGoal] = output;
-	motor[RightMobileGoal] = -power;
+}
+
+void moveRightMogo(int power) {
+	motor[RightMobileGoal] = power;
+}
+
+void moveGoal(int power) {
+	moveLeftMogo(power);
+	moveRightMogo(-power);
 }
 
 void moveSwitchLift(int power) {
 	motor[SwitchLift] = power;
 }
 
-void switch_mogo() {
-	moveMobileGoal(80);
-	while (SensorValue[
+void syncMogo() {
+	while (SensorValue[MogoRightPot] > IN_ANGLE && SensorValue[MogoLeftPot] > IN_ANGLE) {
+		if (SensorValue[MogoRightPot] > IN_ANGLE) {
+			motor[RightMobileGoal] = -SYNC_SPEED;
+		} else {
+			motor[RightMobileGoal] = 0;
+		}
+		if (SensorValue[MogoLeftPot] > IN_ANGLE) {
+			motor[LeftMobileGoal] = -SYNC_SPEED;
+		} else {
+			motor[LeftMobileGoal] = 0;
+		}
+	}
+}
+
+void moveMogoOut() {
+	while (SensorValue[MogoRightPot] < SLOW_ANGLE) {
+		motor[RightMobileGoal] = SLOW_SPEED;
+	}
+	while (SensorValue[MogoRightPot] > OUT_ANGLE) {
+		motor[RightMobileGoal] = FAST_SPEED;
+	}
+}
+
+void moveMogoIn() {
+	while (SensorValue[MogoRightPot] > IN_ANGLE) {
+		moveRightMogo(-FAST_SPEED);
+		moveLeftMogo(FAST_SPEED);
+	}
+	moveRightMogo(0);
+	moveLeftMogo(0);
 }
