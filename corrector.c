a4149235de14b8d35a814debb5dab8de1dedef56
Akhail Kammela <AK9dev@gmail.com>
2017-10-13 20:49:01 -0700

    some pid changes

--- corrector.c
+++ corrector.c
@@ -1,10 +1,5 @@
 #define MoGoKP 1
 
-int initialLeft = SensorValue[MogoLeftPot];
-int initialRight = SensorValue[MogoRightPot];
-
 float getMogoCorrection(int power) {
-	float dleft = SensorValue[MogoLeftPot] - initialLeft;
-	float dright = SensorValue[MogoRightPot] - initialRight;
-	return power ;//+ MoGoKP * (dright - dleft);
+	return power;
 }
--- main.c
+++ main.c
@@ -28,77 +28,79 @@
 #pragma competitionControl(Competition)
 
 #include "motor.h"
-#include "corrector.c"
 #include "pid.c"
 //#include "autonomous.c"
 #include "Vex_Competition_Includes.c"
 
+
+
+bool pidrunning = true;
+
 void pre_auton() {
-datalogClear()
-bStopTasksBetweenModes = true;
+	datalogClear();
+
+	setInitialValues(SensorValue[RightLiftPot], SensorValue[LeftLiftPot]);
+	bStopTasksBetweenModes = true;
 
 
-SensorType[in1] = sensorNone;
-delay(1500);
+	SensorType[in1] = sensorNone;
+	delay(1500);
 
-SensorType[in1] = sensorGyro;
-delay(1500);
+	SensorType[in1] = sensorGyro;
+	delay(1500);
 }
-
-void pre_auton() {}
 
 task autonomous() {
 }
 
 task usercontrol() {
-	datalogClear();
 	pid config;
 	pid rconfig;
+
 	config.kp = 1;
-	config.ki = 0.2;
+	config.ki = 0;
 	config.kd = 0;
 	config.dt = 15;
+	config.min_int = -30;
+	config.max_int = 30;
+	config.min_t = -120;
+	config.max_t = 120;
 
 	rconfig.kp = 1;
-	rconfig.ki = 0.2;
+	rconfig.ki = 0;
 	rconfig.kd = 0;
 	rconfig.dt = 15;
+	rconfig.min_int = -30;
+	rconfig.max_int = 30;
+	rconfig.min_t = -120;
+	rconfig.max_t = 120;
 
 	setLeftConfig(config);
 	setRightConfig(rconfig);
 
-	startLeftLiftPid();
+	startLeftPid();
 
 	while (true) {
 		// drive code
 		moveDrive(vexRT[Ch3], vexRT[Ch2]);
 
-		// lift code
-		if (vexRT[Btn5U]) {
-			stopPid();
-			moveRight(127);
-			} else if (vexRT[Btn5D]) {
-			stopPid();
-			moveRight(-127);
-			} else {
-			if (!isRunning()) {
-				moveRight(0);
-			}
-		}
-
 		if (vexRT[Btn6U]) {
 			openClaw(127);
-			} else if (vexRT[Btn6D]) {
+		}
+		else if (vexRT[Btn6D]) {
 			closeClaw(127);
-			} else {
+		}
+		else {
 			stopClaw();
 		}
 
 		if (vexRT[Btn8L]) {
 			moveGoal(100);
-			} else if (vexRT[Btn8R]) {
+		}
+		else if (vexRT[Btn8R]) {
 			moveGoal(-100);
-			} else {
+		}
+		else {
 			moveGoal(0);
 		}
 
@@ -109,14 +111,43 @@ task usercontrol() {
 			} else {
 			moveSwitchLift(0);
 		}
+
 		if (vexRT[Btn5U]) {
 			stopRightPid();
-			moveRightLift(127);
-			} else if (vexRT[Btn5D]) {
+			if (pidrunning) {
+				moveRightLift(100);
+			}
+			else {
+				moveLift(100);
+			}
+		}
+		else if (vexRT[Btn5D]) {
 			stopRightPid();
-			moveRightLift(-127);
-			} else {
+			if (pidrunning) {
+				moveRightLift(-100);
+			}
+			else {
+				moveLift(-100);
+			}
+		}
+		else {
+			if (pidrunning) {
+				startRightPid(SensorValue[RightLiftPot]);
+			}
+			else {
+				moveLift(0);
+			}
+		}
+
+		if (vexRT[Btn7L]) {
+			stopRightPid();
+			stopLeftPid();
+			pidrunning = false;
+		}
+		else if (vexRT[Btn7R]) {
+			startLeftPid();
 			startRightPid(SensorValue[RightLiftPot]);
+			pidrunning = true;
 		}
 	}
 }
