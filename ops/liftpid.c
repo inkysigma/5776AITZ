036600ceac00ecf4a50ab28515a0be5119c6e561
Justin Ley <inkysigma@outlook.com>
2017-10-23 17:21:20 -0700

    removed legacy for shorter file

--- ops/liftpid.c
+++ ops/liftpid.c
@@ -12,35 +12,6 @@ pid rliftConfig;
 pid * leftConfig;
 pid * rightConfig;
 
-/** Legacy code to ensure stability
- * 
-void setRightConfig(pid * rconfig) {
-	rliftConfig.kp = rconfig->kp;
-	rliftConfig.ki = rconfig->ki;
-	rliftConfig.kd = rconfig->kd;
-	rliftConfig.dt = rconfig->dt;
-	rliftConfig.accumulation = 0;
-	rliftConfig.prev_error = 0;
-	rliftConfig.min_int = rconfig->min_int;
-	rliftConfig.max_int = rconfig->max_int;
-	rliftConfig.min_t = rconfig->min_t;
-	rliftConfig.max_t = rconfig->max_t;
-}
-
-void setLeftConfig(pid * lconfig) {
-	lliftConfig.kp = lconfig->kp;
-	lliftConfig.ki = lconfig->ki;
-	lliftConfig.kd = lconfig->kd;
-	lliftConfig.dt = lconfig->dt;
-	lliftConfig.accumulation = 0;
-	lliftConfig.prev_error = 0;
-	lliftConfig.min_int = lconfig->min_int;
-	lliftConfig.max_int = lconfig->max_int;
-	lliftConfig.min_t = lconfig->min_t;
-	lliftConfig.max_t = lconfig->max_t;
-}
-**/
-
 void setConfig(pid * left, pid * right) {
 	leftConfig = left;
 	rightConfig = right;
